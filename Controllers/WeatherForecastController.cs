fa3e48c00a018de84fbe2aa82291bc7665bad736
Aly Mamdouh <Aly.Mamdouh@InteractTS.com>
2023-03-08 00:26:01 +0200

    inject _logger in controller and put in function

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
 
 namespace LoggingWithSerilog.Controllers
 {
@@ -21,13 +23,23 @@ namespace LoggingWithSerilog.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            //{
+            //    Date = DateTime.Now.AddDays(index),
+            //    TemperatureC = Random.Shared.Next(-20, 55),
+            //    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            //})
+            //.ToArray();
+
+            _logger.LogDebug("Inside GetWeatherForecast endpoint");
+            var response = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+            }).ToArray();
+            _logger.LogDebug($"The response for the get weather forecast is {JsonConvert.SerializeObject(response)}");
+            return response;
         }
     }
 }
