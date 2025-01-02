3371fb257be0f061bc4c990ed9e97693ad14aab6
Aly Mamdouh <aly.mamdouh@interactts.com>
2025-01-02 12:17:50 +0200

    writing our application logs to an external service, SEQ.

--- dotNet8/SerilogInDot8/example/DummyService.cs
+++ dotNet8/SerilogInDot8/example/DummyService.cs
@@ -15,5 +15,11 @@
             logger.LogDebug("nothing much");
             logger.LogInformation("Invoking {@Event} with ID as {@Id}", "SomeEvent", Guid.NewGuid());
         }
+        ///    config seq 
+        ///    in cmd as admin 
+        ///    - docker run --name seq -d -e ACCEPT_EULA=Y -p 80:80 -p 5341:5341 datalust/seq
+        ///    then in same cmd 
+        ///    - docker run --name View_Serilog_Messages -d --restart unless-stopped -e ACCEPT_EULA=Y -p 5341:80 datalust/seq:latest
+
     }
 }
